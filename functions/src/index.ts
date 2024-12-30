e34c8f89c317cd9873f0be1f078e933dcecf2dd4
nopops <nopops@nightofpassion.seq>
2024-12-30 15:21:05 +0100

    new trigger for new auth user, add user to sendgrid

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,10 +3,12 @@ import { sendNewUserNotificationEmail } from "./triggers/newUser";
 import { sendNewEvenParticipantNotification } from "./triggers/newEventParticipant";
 import { sendPendingValidationRequest } from "./triggers/pendingValidationRequestNotification";
 import { newStatusNotifyUser } from "./triggers/newStatusNotifyUser";
+import { newUserAuth } from "./triggers/newAuthUser";
 
 export default {
   sendNewEvenParticipantNotification,
   sendNewUserNotificationEmail,
   sendPendingValidationRequest,
   newStatusNotifyUser,
+  newUserAuth,
 };
--- functions/src/lib/mail/sendgrid.ts
+++ functions/src/lib/mail/sendgrid.ts
@@ -1,8 +1,11 @@
 import { MailDataRequired } from "@sendgrid/mail";
 import * as sgMail from "@sendgrid/mail";
+import * as client from "@sendgrid/client";
+import { ClientRequest } from "@sendgrid/client/src/request";
 
 export const init = (sendgridApiKey: string) => {
   sgMail.setApiKey(sendgridApiKey);
+  client.setApiKey(sendgridApiKey);
 };
 
 
@@ -17,4 +20,35 @@ export const sendSgMail = (mailMsg: MailDataRequired) => {
       }
     }
   );
+};
+
+export const updateSgContact = (email: string, listId: string) => {
+  console.log("updateSgContact--------------------");
+  console.log("client", client);
+
+  const data = {
+    list_ids: [listId],
+    contacts: [
+      {
+        email: email,
+      }],
+  };
+
+  console.log("data:", data);
+  const request: ClientRequest = {
+    url: "/v3/marketing/contacts",
+    method: "PUT",
+    body: data,
+  };
+
+  client
+    .request(request)
+    .then(([response, body]) => {
+      console.log(response.statusCode);
+      console.log(response.body);
+      console.log(body);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
