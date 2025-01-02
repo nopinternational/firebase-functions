d8e589cb8ec97364ac004d617830828d0d49d6d4
nopops <nopops@nightofpassion.seq>
2025-01-02 11:28:29 +0100

    getting the api key for sendgrid

--- functions/src/triggers/newAuthUser.ts
+++ functions/src/triggers/newAuthUser.ts
@@ -1,33 +1,21 @@
 import { auth } from "firebase-functions/v1";
-import { defineSecret, defineString } from "firebase-functions/params";
+import { defineString } from "firebase-functions/params";
 import { init, updateSgContact } from "../lib/mail/sendgrid";
 
-const sendgridAPIKey = defineSecret("SENDGRID_API_KEY");
+
+// Should be a secret but genv1 cannot handle secrets, need to be included into dependency array
+const sendgridAPIKey = defineString("SENDGRID_API_KEY");
 const sendgridNopListId = defineString("SENDGRID_NOP_LISTID");
 
 
 export const newUserAuth = auth.user().onCreate((user) => {
-  // ...
-  // const gmailEmail = paramGEmail.value();
-  // const gmailPassword = paramGPassword.value();
-  // console.log("beforeUserCreated", event.data?.uid, gEmail.value());
   console.log("UserCreated", user);
   console.log("UserCreated.email", user.email);
-  // console.log(gmailEmail, gmailPassword);
 
   if (user.email) {
     onNewUserCreated(user.email);
   }
 });
-
-// export const newUserAuthDatabase =
-//   onValueWritten({ ref: "/validation/{userId}/current/email", secrets: [sendgridAPIKey] }, (event) => {
-//     console.log("newStatusNotifyUser", event);
-//     console.log("event.params.userId", event.params.userId);
-//     console.log("-----event.data.before.val()", event.data.before.val());
-//     console.log("+++++event.data.after.val()", event.data.after.val());
-//     // onNewUserCreated(event.data.after.val());
-//   });
 
 const onNewUserCreated = (email: string) => {
   console.log(sendgridAPIKey.value(), sendgridNopListId.value());
